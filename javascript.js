e12ccfe4c3bf959bb72f7a0bf93df6a2a256194e
wjj2329 <wjj2329@gmail.com>
2016-02-05 15:52:04 -0700

    The code
    The Project

---
+++ javascript.js
@@ -0,0 +1,39 @@
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      avatarUrl: user.url
+    }); 
+    
+    user.name = ''; 
+    user.url = '';
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+      user: '='  
+    },
+    restrict: 'E', 
+    template: (
+      '<div class="Avatar">' + '<img ng-src="{{user.avatarUrl}}" />' +'<h4>{{user.name}}</h4>' +
+      '</div>'
+    ), 
+    link: link
+  };
+  
+  function link (scope) { 
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'http://thealmanac.org/assets/img/default_avatar.png';
+    }
+  }
+
+}
